26d71aa9b2e3357eeb55439ec1eca80c27ad95f1
geduardo <eduardo.gonzalez.sanchez07@gmail.com>
2019-11-19 05:20:13 +0100

    getting unitary operator

--- AddGate.py
+++ AddGate.py
@@ -1,49 +1,34 @@
-
-# coding: utf-8
-
-# In[3]:
-
-
 from qiskit import *
 from qiskit.tools.visualization import plot_histogram
 import numpy as np
-
-
-# In[4]:
-
+from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
+from qiskit import execute, BasicAer
+from qiskit.compiler import transpile
+from qiskit.quantum_info.operators import Operator, Pauli
+from qiskit.quantum_info import process_fidelity
+from qiskit.extensions import RXGate, CnotGate, XGate
 
 def addGate(input):
-
     q = QuantumRegister(1) # a qubit in which to encode and manipulate the input
     c = ClassicalRegister(1) # a bit to store the output
     qc = QuantumCircuit(q, c) # this is where the quantum program goes
-    
-
     if input=='X':
         qc.x(q[0])
-
-    qc.measure(q[0], c[0])
-    
-    # We'll run the program on a simulator
-    backend = Aer.get_backend('qasm_simulator')
-    # Since the output will be deterministic, we can use just a single shot to get it
-    job = execute(qc,backend,shots=1)
-    output = next(iter(job.result().get_counts()))
-    
+    if input=='H':
+        qc.h(q[0])
+    if input=='Y':
+        qc.y(q[0])
+    output = Operator(qc)
     return output
 
-
-# In[15]:
-
-
-print("Initial state: |0⟩")
-print("Final state: |1⟩")
-
-x = input("Input a correct gate: ")
-result = addGate(x)
+print("Final state: 1/sqrt(2)*(|0⟩+|1⟩)")
+a = input("Input a gate: ")
+result = addGate(a)
 print("output is ", result)
-if result == "1":
-    print("correct")
-else:
-    print("incorrect")
+
+
+# if result == "1":
+#     print("correct")
+# else:
+#     print("incorrect")
 
